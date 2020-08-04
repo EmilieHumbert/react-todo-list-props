c521abdc2520f0c185eaf737cdf0ea9905115ab6
emilie <emi.m.humbert@googlemail.com>
2020-08-04 13:36:45 +0100

    update todo list display

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,14 +1,22 @@
 import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
 import CheckboxToggle from "./CheckboxToggle";
 
 const Todo = ({ todo, updateTodo }) => {
   const updateChecked = (newValue) => updateTodo(todo, "completed", newValue);
   return (
-    <li>
-      <CheckboxToggle checked={todo.completed} updateChecked={updateChecked} />
-      <span>{todo.text}</span>
-    </li>
+    <ListItem>
+      <ListItemIcon>
+        <CheckboxToggle
+          checked={todo.completed}
+          updateChecked={updateChecked}
+        />
+      </ListItemIcon>
+      <ListItemText primary={todo.text} />
+    </ListItem>
   );
 };
 
